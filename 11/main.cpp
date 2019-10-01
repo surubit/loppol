80375da2b7a936d43e21a08a3c70d6cdee987bf0
Nikita Horb <masternik007@gmail.com>
2019-10-01 16:56:18 +0300

    add Vectors

--- 11/main.cpp
+++ 11/main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 int main()
 {
     int a,b;
     cin >> a >> b;
-
+        vector<int> v(a,b);
+        for(int i = 0; i < a;i++){
+            cin >> v[i];
+        }
     return 0;
 }
